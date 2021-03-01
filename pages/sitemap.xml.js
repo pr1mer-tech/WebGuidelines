e0dd1463bc1aa54f15a487daa37cbcfc97eb59f8
Arthur Guiot <arguiot@gmail.com>
2021-02-28 22:54:51 -0500

    Optimized for Cloudflare Pages

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const createSitemap = (pages) => `<?xml version="1.0" encoding="UTF-8"?>
+export const createSitemap = (pages) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>https://guidelines.pr1mer.tech/</loc>
